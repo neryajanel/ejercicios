db8600effd89d0e2ac11ea47ece5ff256bafbb05
nizan <nizan@debian.debian>
2020-12-01 17:17:47 -0500

    first commit

---
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -0,0 +1,41 @@
+def add(a, b)
+  # return the result of adding a and b
+  a * b
+end
+puts add(2, 4)
+puts add(2, 4)
+puts add(2, 4)
+puts add(2, 4)
+puts add(2, 4)
+
+def subtract(a, b)
+  # return the result of subtracting a from b
+end
+
+def multiply(a, b)
+  # return the result of multiplying a times b
+end
+
+def divide(a, b)
+  # return the resut of dividing a by b
+end
+
+def remainder(a, b)
+  # return the remainder of dividing a by b using the modulo operator
+end
+
+def float_division(a, b)
+  # return the result of dividing a by b as a float, rather than an integer
+end
+
+def string_to_number(string)
+  # return the result of converting a string into an integer
+end
+
+def even?(number)
+  # return true if the number is even
+end
+
+def odd?(number)
+  # return true if the number is odd
+end
